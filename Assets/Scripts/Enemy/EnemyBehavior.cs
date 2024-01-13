a945454978dc369644789e1b00dab7cc595e95e5
Zeyu Huang <zeze33629@gmail.com>
2024-01-13 13:22:54 -0500

    Start of enemy

---
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public bool isAggro;
+    public NavMeshAgent agent;
+    public float maxAngle = 45;
+    public float maxDistance = 10;
+    public float timer = 1.0f;
+    public float visionCheckRate = 1.0f;
+    public float speed;
+    public Transform[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
